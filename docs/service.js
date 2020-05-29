91ca12d07fa8be3a153e91546c54bca0cba92555
Patrick Griffin <58729+firien@users.noreply.github.com>
2020-05-28 22:39:30 -0400

    remove coffeescript

--- docs/service.js
+++ docs/service.js
@@ -1,48 +1,66 @@
-(function() {
-  var $cacheName, $prefix, $urls, clearPreviousCaches, tag;
+const tag = '11';
+const $prefix = 'SCONCE';
+const $cacheName = `${$prefix}-${tag}`;
 
-  tag = '10';
+const $urls = [
+  
+  'bundle.1adc7297d4b2947102c3.js',
+  
+  'javascripts/sconce.efbb980e2fa3900f422a.js',
+  
+  'stylesheets/index.fb0cad18a722a1b252b5.css',
+  
+  'images/icon-152.16a460569578cca83edb.png',
+  
+  'images/icon-167.e8c0e1abca38bc2ee6c6.png',
+  
+  'images/icon-180.678f79f1f40ac2f40ba1.png',
+  
+  'images/icon-192.c30f2a2017130f98365a.png',
+  
+  'images/icon-512.7bc7986d2ed3d788782a.png',
+  
+  'pwa.ba451b196f9dce938197.js',
+  
+  'manifest.webmanifest',
+  
+  'index.html',
+  
+  '/',
+  
+];
 
-  $prefix = 'SCONCE';
+self.addEventListener('install', async (event) => {
+  let cache = await event.waitUntil(caches.open($cacheName));
+  await cache.addAll($urls);
+})
 
-  $cacheName = `${$prefix}-${tag}`;
+const clearPreviousCaches = async () => {
+  let keys = await caches.keys()
+  keys = keys.filter((key) => {
+    return (key != $cacheName) && key.startsWith($prefix)
+  })
+  for (let key of keys) {
+   await caches.delete(key);
+  }
+}
 
-  $urls = ['/sconce/bundle.3af26372e064b4fdc89a.js', '/sconce/javascripts/sconce.30dd45b734b623511cb4.js', '/sconce/stylesheets/index.fb0cad18a722a1b252b5.css', '/sconce/images/icon-152.16a460569578cca83edb.png', '/sconce/images/icon-167.e8c0e1abca38bc2ee6c6.png', '/sconce/images/icon-180.678f79f1f40ac2f40ba1.png', '/sconce/images/icon-192.c30f2a2017130f98365a.png', '/sconce/images/icon-512.7bc7986d2ed3d788782a.png', '/sconce/pwa.40e7f83f095c9527ba5d.js', '/sconce/manifest.webmanifest', '/sconce/index.html', '/sconce/'];
+self.addEventListener('activate', (event) => {
+  return event.waitUntil(clearPreviousCaches())
+})
 
-  self.addEventListener('install', function(event) {
-    return event.waitUntil(caches.open($cacheName).then(function(cache) {
-      return cache.addAll($urls);
-    }));
-  });
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.open($cacheName).then((cache) => {
+      return cache.match(event.request, {ignoreSearch: true})
+    }).then((response) => {
+      return response || fetch(event.request)
+    })
+  )
+})
 
-  clearPreviousCaches = function() {
-    return caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(key) {
-        return (key !== $cacheName) && key.startsWith($prefix);
-      }).map(function(key) {
-        return caches.delete(key);
-      }));
-    });
-  };
-
-  self.addEventListener('activate', function(event) {
-    return event.waitUntil(clearPreviousCaches());
-  });
-
-  self.addEventListener('fetch', function(event) {
-    return event.respondWith(caches.open($cacheName).then(function(cache) {
-      return cache.match(event.request, {
-        ignoreSearch: true
-      });
-    }).then(function(response) {
-      return response || fetch(event.request);
-    }));
-  });
-
-  self.addEventListener('message', function(event) {
-    if (event.data.action === 'skipWaiting') {
-      return self.skipWaiting();
-    }
-  });
-
-}).call(this);
+self.addEventListener('message', (event) => {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
+})
