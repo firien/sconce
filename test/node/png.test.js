e24e52df23c1a350357df47ea651cbda35e53baf
Patrick Griffin <58729+firien@users.noreply.github.com>
2022-11-19 08:33:23 -0500

    move node tests

--- test/node/png.test.js
+++ test/node/png.test.js
@@ -1,6 +1,6 @@
 import test from 'node:test'
 import assert from 'node:assert/strict'
-import pngDimensions from '../javascripts/png.js'
+import pngDimensions from '../../javascripts/png.js'
 import { readFileSync } from 'node:fs'
 import { fileURLToPath } from 'url'
 import { dirname, join } from 'path'
@@ -8,14 +8,14 @@ import { dirname, join } from 'path'
 const __dirname = dirname(fileURLToPath(import.meta.url))
 
 test('png dimensions test 1', () => {
-  let buffer = readFileSync(join(__dirname, '../images/icon-152.png'))
+  let buffer = readFileSync(join(__dirname, '../../images/icon-152.png'))
   let png = pngDimensions(new Uint8Array(buffer).buffer)
   assert.strictEqual(152, png.width)
   assert.strictEqual(152, png.height)
 })
 
 test('png dimensions test 2', () => {
-  let buffer = readFileSync(join(__dirname, '../images/icon-192.png'))
+  let buffer = readFileSync(join(__dirname, '../../images/icon-192.png'))
   let png = pngDimensions(new Uint8Array(buffer).buffer)
   assert.strictEqual(192, png.width)
   assert.strictEqual(192, png.height)
